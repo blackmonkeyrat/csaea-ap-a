860631edd414542b28461b48663f868624e18eca
blackmonkeyrat <singh20288006@student.nhvweb.net>
2026-09-10 13:08:16 +0000

    basic class skeleton

---
+++ BasicClass.java
@@ -0,0 +1,15 @@
+public class BasicClass {
+     public static void main(String[] args) {
+
+            // Attributes / Data
+            // Variables
+
+            // Constructor --- SPECIAL METHOD called every time you make an object
+            // Same name as the class, followed by parenthesis
+            // BasicClass()
+
+            
+            // Behaviors / Methods
+            // You can write as many as you want
+    }
+}
